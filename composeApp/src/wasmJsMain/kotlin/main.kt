56fe8f5b5367eb0bf0394c4f1aebe0f268a10271
jacobrein <jacob.rein@gabb.com>
2024-08-15 07:38:47 -0600

    Update favicon in WASM JS main
    Added code to set the favicon dynamically by querying or creating a link element and setting its "href" attribute to a specific URL. This ensures the favicon is correctly displayed when the application runs in a web environment.

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -5,6 +5,15 @@ import kotlinx.browser.document
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    val link = document.querySelector("link[rel~='icon']")
+        ?: document.createElement("link").apply {
+            setAttribute("rel", "icon")
+            document.head?.appendChild(this)
+        }
+    link.setAttribute(
+        "href",
+        "https://github.com/jakepurple13/TwentyFourGame/blob/master/composeApp/src/androidMain/ic_launcher-playstore.png?raw=true"
+    )
     ComposeViewport(document.body!!) {
         App(remember { Settings { "" } })
     }
